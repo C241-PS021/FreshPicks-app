8a5e1fd3dc13d8c2fffe962a816cb256cca6424b
Hikimiko01 <bimapramudya26@gmail.com>
2024-06-05 22:28:02 +0700

    Updated xml design

--- app/src/main/java/com/mobile/freshpicks/MainActivity.kt
+++ app/src/main/java/com/mobile/freshpicks/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.mobile.freshpicks
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.mobile.freshpicks.databinding.ActivityMainBinding
+import com.mobile.freshpicks.view.detect.DetectFragment
+import com.mobile.freshpicks.view.home.HomeFragment
+import com.mobile.freshpicks.view.savedresult.SavedResultFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.navView.background = null
+        replaceFragment(HomeFragment())
+
+        binding.navView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> replaceFragment(HomeFragment())
+                R.id.navigation_saved -> replaceFragment(SavedResultFragment())
+
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
--- app/src/main/java/com/mobile/freshpicks/view/savedresult/SavedResultFragment.kt
+++ app/src/main/java/com/mobile/freshpicks/view/savedresult/SavedResultFragment.kt
@@ -1,25 +1,25 @@
-package com.mobile.freshpicks
+package com.mobile.freshpicks.view.savedresult
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mobile.freshpicks.R
 
-
-class HomeFragment : Fragment() {
-
+class SavedResultFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved_result, container, false)
     }
 
 }
